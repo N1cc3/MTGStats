e7e899201b596ef1b963eef855dc444672d3d2f5
N1cc3 <niclaslindgren91@hotmail.com>
2017-04-15 02:24:12 +0300

    Renamed matchHistory to history. Cookies and json parse first test.

--- js/history.js
+++ js/history.js
@@ -1,29 +1,30 @@
-var debugMatches = [
+var debugMatches = JSON.stringify([
   {
-    redName: "Maick",
-    blueName: "Nicce",
-    winner: "red"
+    players: ['Maick','Nicce'],
+    winner: 0
   }
-];
+]);
 
-COOKIE.set("matches", debugMatches, 1);
+COOKIE.set('matches', debugMatches, 1);
 
 var matches = COOKIE.get("matches");
 
-for (match of matches) {
-  addMatch(match);
+function addMatches(matches) {
+  var matchesElement = document.getElementById('matches');
+  var fragment = document.createDocumentFragment();
+
+  for (match of matches) {
+    var matchElement = document.createElement('div');
+    matchElement.className = 'match';
+    var players = match.players;
+    for (var i = 0; i < players.length; i++) {
+      var playerElement = document.createElement('div');
+      playerElement.className = 'player';
+      playerElement.setAttribute('player', i);
+      if (match.winner = i) playerElement.setAttribute('winner', '');
+      playerElement.textContent = players[i];
+    }
+  }
 }
 
-function addMatch(match) {
-  var matchesElement = document.getElementById('matches');
-  var fragment = 
-}
-    <div id="matches" class="historyBox">
-<div class="match">
-  <div class="playerName red winner">
-    Nicce
-  </div>
-  <div class="playerName blue">
-    Maick
-  </div>
-</div>
+addMatches(matches);
