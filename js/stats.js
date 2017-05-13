eae48caff6cc9fcd15335d5b8bbd2998b1b2e5b9
N1cc3 <niclaslindgren91@hotmail.com>
2017-05-13 23:46:50 +0300

    Added getDistance function.

--- js/stats.js
+++ js/stats.js
@@ -68,6 +68,12 @@ function getAngle(x1, y1, x2, y2) {
   return Math.atan2(x2 - x1, y2 - y1);
 }
 
+function getDistance(x1, y1, x2, y2) {
+  var dx = x2 - x1;
+  var dy = y2 - y1;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
 //////////////////////////
 // DRAGCHANGE ELEMENTS  //
 //////////////////////////
