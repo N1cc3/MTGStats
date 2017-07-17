8ac80c15940dc347a59a6be3c79149343e47e1d9
N1cc3 <niclaslindgren91@hotmail.com>
2017-07-17 20:58:55 +0300

    Added slider to clear history.

--- js/history.js
+++ js/history.js
@@ -1,6 +1,8 @@
 /*jshint esversion: 6 */
 
 var matches = JSON.parse(localStorage.getItem('matches'));
+var BODY = document.getElementById('body');
+var SLIDER = document.getElementById('slider');
 
 function addMatches(matches) {
   var historyBox = document.getElementById('historyBox');
@@ -25,3 +27,68 @@ function addMatches(matches) {
 }
 
 addMatches(matches);
+
+function clearHistory() {
+  var matchesString = "[]";
+  localStorage.setItem('matches', matchesString);
+
+  var matchElements = document.getElementsByClassName('match');
+  for (matchElement of matchElements) {
+    matchElement.parentNode.removeChild(matchElement);
+  }
+}
+
+function resetSlider() {
+  SLIDER.setAttribute('reset', '');
+  SLIDER.addEventListener('transitionend', function(e) {
+    SLIDER.removeAttribute('reset');
+  });
+  SLIDER.style.removeProperty('width');
+}
+
+SLIDER.addEventListener('mousedown', function(e) {
+  if (event.which != 1) {
+    return;
+  }
+  SLIDER.removeAttribute('reset');
+  var startX = e.pageX;
+  var startWidth = SLIDER.offsetWidth;
+  BODY.onmousemove = function(e) {
+    SLIDER.style.width = startWidth + (e.pageX - startX) + 'px';
+    if (SLIDER.offsetWidth >= BODY.offsetWidth) {
+      clearHistory();
+      resetSlider();
+      BODY.onmousemove = null;
+    }
+  };
+
+  BODY.addEventListener('mouseup', function(e) {
+    if (event.which != 1) {
+      return;
+    }
+    BODY.onmousemove = null;
+    resetSlider();
+  });
+});
+
+SLIDER.addEventListener('touchstart', function(e) {
+  SLIDER.removeAttribute('reset');
+  var startX = e.changedTouches.item(0).pageX;
+  var startWidth = SLIDER.offsetWidth;
+  registerEventListener(BODY, {
+    event: 'touchmove',
+    callback: function(e) {
+      SLIDER.style.width = startWidth + (e.changedTouches.item(0).pageX - startX) + 'px';
+      if (SLIDER.offsetWidth >= BODY.offsetWidth) {
+        clearHistory();
+        resetSlider();
+        unRegisterAllEventListeners(BODY);
+      }
+    }
+  });
+
+  BODY.addEventListener('touchend', function(e) {
+    resetSlider();
+    unRegisterAllEventListeners(BODY);
+  });
+});
