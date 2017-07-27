07db145dcc60db94149c8c0a0dc96055fb0e8a0f
N1cc3 <niclaslindgren91@hotmail.com>
2017-07-27 23:18:23 +0300

    Fixed serviceWorker path.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
+    navigator.serviceWorker.register('js/serviceWorker.js').then(function(registration) {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
       console.log('ServiceWorker registration failed: ', err);
