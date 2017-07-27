c3dded2804962c0b770b632cb56ba7bde448a9e5
N1cc3 <niclaslindgren91@hotmail.com>
2017-07-27 23:14:36 +0300

    First test of service worker.

---
+++ js/serviceWorker.js
@@ -0,0 +1,76 @@
+var CACHE_NAME = 'MTGStats-cache-v1';
+var urlsToCache = [
+  '/css/history.css',
+  '/css/index.css',
+  '/css/master.css',
+  '/css/stats.css',
+  '/js/draw.js',
+  '/js/history.js',
+  '/js/popup-component.js',
+  '/js/slider-component.js',
+  '/js/stats.js',
+  '/click.mp3',
+  '/2player.html',
+  '/4player.html',
+  '/history.html',
+  '/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['MTGStats-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
