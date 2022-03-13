d3217837d6e82ff52d58b46c38d8f5cd4068d20e
Niclas Lindgren <niclaslindgren91@hotmail.com>
2022-03-14 00:06:01 +0200

    Update cache v1.3.0

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'MTGStats-cache-v1.2.2';
+var CACHE_NAME = 'MTGStats-cache-v1.3.0'
 var urlsToCache = [
   './css/history.css',
   './css/index.css',
@@ -24,64 +24,58 @@ var urlsToCache = [
   './2player.html',
   './4player.html',
   './history.html',
-  '.'
-];
+  '.',
+]
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Opened cache')
+      return cache.addAll(urlsToCache)
+    })
+  )
+})
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response
+      }
+
+      var fetchRequest = event.request.clone()
+
+      return fetch(fetchRequest).then(function (response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response
         }
 
-        var fetchRequest = event.request.clone();
+        var responseToCache = response.clone()
 
-        return fetch(fetchRequest).then(
-          function(response) {
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseToCache)
+        })
 
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
+        return response
       })
-    );
-});
+    })
+  )
+})
 
-self.addEventListener('activate', function(event) {
-
-  var cacheWhitelist = [CACHE_NAME];
+self.addEventListener('activate', function (event) {
+  var cacheWhitelist = [CACHE_NAME]
 
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
+        cacheNames.map(function (cacheName) {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
+            return caches.delete(cacheName)
           }
         })
-      );
+      )
     })
-  );
-});
+  )
+})
